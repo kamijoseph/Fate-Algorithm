ad196ccc76e648ca185b659167270f37742dd1f1
Kami Joseph <josephkiarie561@gmail.com>
2025-01-05 17:20:46 +0300

    Welcome User Mod

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # Main to run the several modules of the project
+
+# Welcome Message
+print("Welcome to the Fate Algorithm System\nMy name is Fate, and I will help you with different tasks and terminal gameplays\n")
+name = input("What is your name? ")
+print(f"Nice to meet you {name}!\n")
