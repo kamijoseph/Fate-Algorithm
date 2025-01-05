d6ae463f99bdb22bc49d912f366634536dd9dc42
Kami Joseph <josephkiarie561@gmail.com>
2025-01-05 18:27:07 +0300

    distance converter

--- measurement_calc.py
+++ measurement_calc.py
@@ -1,9 +1,21 @@
 
 # Units Of Measurement Conversion Calculator
+
+# Distance Converter
 def distance_converter():
     unit = input("Is the distance in Miles or kilometers? ").strip().lower()
     try:
-        distancee = float(input("Enter the distance: "))
+        distance = float(input("Enter the distance (e.g 5.0/ 6.3): "))
     except ValueError:
         print("Invalid Number.Please enter a valid numerical value")
+        return
+    
+    if unit == "miles":
+        converted_distance = round(distance * 1.60934, 2)
+        print(f"{distance} miles is equal to {converted_distance * 1.60934} kilometers")
+    elif unit == "kilometers":
+        converted_distance = round(distance / 1.60934, 2)
+        print(f"{distance} kilometers is equal to {converted_distance / 1.60934} miles")
+    else:
+        print("Invalid unit. Please enter either miles or kilometers")
         return
