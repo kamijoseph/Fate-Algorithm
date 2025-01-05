45f719ec2c39161dfb118a44d2a8e30403eae295
Kami Joseph <josephkiarie561@gmail.com>
2025-01-05 20:02:12 +0300

    Modules Configured In Main

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 # Main to run the several modules of the project
+# The Modules are:
+import Arithmetic_calc
+import binomial_distribution
+import compound_interest
+import countdown
+import fibbonacci
+import math_challenge
+import mean_median
 import measurement_calc
+import number_guess
+import password_gen
+import pigGame
+import quiz_game
+import rock_paper_scissors
+import slot_machine
+import stocksPrice
+import turtle_race
+import YouTube
 
 # Welcome Message
 print("Welcome to the Fate Algorithm System\nMy name is Fate, and I will help you with different tasks and terminal gameplays\n")
 name = input("What is your name? ")
 print(f"Nice to meet you {name}!\n")
 
+# Main Menu
+print(Arithmetic_calc.main())
+print(binomial_distribution.main())
+print(compound_interest.main())
+print(countdown.main())
+print(fibbonacci.main())
+print(math_challenge.main())
+print(mean_median.main())
 print(measurement_calc.main())
+print(number_guess.main())
+print(password_gen.main())
+print(pigGame.main())
+print(quiz_game.main())
+print(rock_paper_scissors.main())
+print(slot_machine.main())
+print(stocksPrice.main())
+print(turtle_race.main())
+print(YouTube.main())
