6d9baab4a750bc0c904144fb69c40f029877fc12
Kami Joseph <josephkiarie561@gmail.com>
2025-01-06 16:47:09 +0300

    Square Root

--- Arithmetic_calc.py
+++ Arithmetic_calc.py
@@ -2,3 +2,16 @@
 # Simple Aritmetic Calculator
 import math
 
+def calculate():
+    try:
+        num1 = float(input("Enter the first number"))
+        operator = input("Enter the operator (+, -, *, /, %, ^, sqrt): ").strip()
+        if operator.lower() == "sqrt":
+            result = math.sqrt(num1)
+            print(f"The square root of {num1} is {result}")
+            
+        
+    except ValueError:
+        print("Invalid Input! Enter a vali numerical number.")
+        return
+    
