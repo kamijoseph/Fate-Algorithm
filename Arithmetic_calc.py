638e6155bf485893ed0f0b297c2cb5e216f86a87
Kami Joseph <josephkiarie561@gmail.com>
2025-01-06 16:57:04 +0300

    operators

--- Arithmetic_calc.py
+++ Arithmetic_calc.py
@@ -9,6 +9,28 @@ def calculate():
         if operator.lower() == "sqrt":
             result = math.sqrt(num1)
             print(f"The square root of {num1} is {result}")
+        else:
+            num2 = float(input("Enter the second number: "))
+            if operator == "+":
+                result = num1 + num2
+            elif operator == "-":
+                result = num1 - num2
+            elif operator == "*":
+                result = num1 * num2
+            elif operator == "/":
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    print("You cannot divide by zero!")
+                    return
+            elif operator == "%":
+                result = num1 % num2
+            elif operator == "^":
+                result = num1 ** num2
+            else:
+                print("Invalid operators! Supported operators are +, -, *, /, %, ^, sqrt")
+                return
+            
             
         
     except ValueError:
