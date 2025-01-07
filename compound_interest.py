d9b8fc592108a1ef6434de6661722b6e45c07360
Kami Joseph <josephkiarie561@gmail.com>
2025-01-07 17:37:57 +0300

    CIC

--- compound_interest.py
+++ compound_interest.py
@@ -1,2 +1,13 @@
-def compound_interest():
-    print("Welcome. This is the Compound Interest Calculator")
+
+# Simple Compound Interest Calculator
+def get_positive_float(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value  > 0:
+                return value
+            else:
+                print("Value must be greater than zero. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
+
