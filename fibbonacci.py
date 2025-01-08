2d98707daa36b091617a5217a15925389eedea09
Kami Joseph <josephkiarie561@gmail.com>
2025-01-08 08:35:41 +0300

    sequence

--- fibbonacci.py
+++ fibbonacci.py
@@ -1,2 +1,10 @@
 
 # Simple program to generate fibonacci numbers
+
+def fib_generator(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
