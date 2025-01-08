1e1cedad2aa07a93f61b4208bdb47ed243f4e3e6
Kami Joseph <josephkiarie561@gmail.com>
2025-01-08 09:05:54 +0300

    Rock Paper Scissors init

---
+++ rock_paper_scissors.py
@@ -0,0 +1,16 @@
+
+# Rock, Paper, Scissors Game
+import random
+
+# Rock, Paper, Scissors Game Function
+def rps_game():
+    choices = ["rock", "paper", "scissors"]
+    player = None
+    computer = random.choice(choices)
+
+# Man Function
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
