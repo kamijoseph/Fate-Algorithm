b4ffb1a9b2130453a4a4d2380a6be1da08e60deb
Kami Joseph <josephkiarie561@gmail.com>
2025-01-08 08:43:57 +0300

    Main function

--- fibbonacci.py
+++ fibbonacci.py
@@ -8,3 +8,18 @@ def fib_generator(n):
         sequence.append(a)
         a, b = b, a + b
     return sequence
+
+def main():
+    print("Welcome, this is the Fibonacci Number Generator")
+    while True:
+        try:
+            n = int(input("Enter the number of fibonacci numbers you want to generate: "))
+            if n < 1:
+                print("Please enter a number greater tan zero.")
+                continue
+            else:
+                result = fib_generator(n)
+                print(f"The first {n} Fibonacci numbers are: {result}")
+                break
+        except ValueError:
+            print("Invalid input! Pleae enter a valid number.")
