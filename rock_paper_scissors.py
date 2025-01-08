16585e9862e9bad0fee74af5c50d009c3c7c566b
Kami Joseph <josephkiarie561@gmail.com>
2025-01-08 09:15:25 +0300

    Prompt function

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,9 +4,17 @@ import random
 
 # Rock, Paper, Scissors Game Function
 def rps_game():
-    choices = ["rock", "paper", "scissors"]
-    player = None
-    computer = random.choice(choices)
+    max_games = 3
+    comp_score =0
+    player_score = 0
+    
+    for _ in range(max_games):
+        choices = ["rock", "paper", "scissors"]
+        player = None
+        computer = random.choice(choices)
+        while player not in choices:
+            player = input("Rock, Paper, or Scissors?: ").lower()
+        print(f"Player: {player}\nComputer: {computer}")
 
 # Man Function
 def main():
