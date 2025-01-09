c16f5f10de5ac841fd22560c7304ac70521d4710
Kami Joseph <josephkiarie561@gmail.com>
2025-01-09 09:40:56 +0300

    Gameplay Function

--- number_guess.py
+++ number_guess.py
@@ -1,11 +1,31 @@
 
 # Number Guessing Game
-def number_guess():
-    pass
+import random
 
+# Game Play Function
+def number_guess():
+    number = random.randint(1, 100)
+    
+    while True:
+        try:
+            user_guess = int(input("Enter Your Guess: "))
+            if user_guess < number:
+                print("Too low. Try Again.")
+            elif user_guess > number:
+                print("Too high. Try Again.")
+            else:
+                print("That is correct!")
+                break
+        except ValueError:
+            print("Invalid input. Enter a valid numerical number.")
+    
+    play_again()
+
+# lay again Function
 def play_again():
     pass
 
+# Main Function
 def main():
     pass
 
