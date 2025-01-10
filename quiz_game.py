2635ee45ebb357d80d93d7950504e312f6455a24
Kami Joseph <josephkiarie561@gmail.com>
2025-01-10 10:16:53 +0300

    Initializing A new Game function

--- quiz_game.py
+++ quiz_game.py
@@ -1,9 +1,29 @@
 
 # Simple Quiz Game just for logic
 def new_game():
-    pass
+    
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+    
+    for key in questions:
+        print("-------------------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D): ").upper()
+        guesses.append(guess)
+        
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num += 1
+        
+        display_score(correct_guesses, guesses)
+    
 
 def check_answer():
+    pass
+
+def display_score(correct_guesses, guesses):
     pass
 
 def play_again():
