181b70c1741b424327060b1005de190e9d82dc6d
Kami Joseph <josephkiarie561@gmail.com>
2025-01-10 10:39:59 +0300

    Play Again Function

--- quiz_game.py
+++ quiz_game.py
@@ -34,7 +34,20 @@ def display_score(correct_guesses, guesses):
     print("-----------------------------")
     print("RESULTS")
     print("-----------------------------")
+    
+    print("Answers: ", end=" ")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+    print("Guesses: ", end=" ")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+    
+    score = int((correct_guesses/len(questions))*100)
+    print(f"Your Score is {str(score)}%")
 
+# Function to Prompt Play Again 
 def play_again():
     pass
     
