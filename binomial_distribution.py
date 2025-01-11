ce713a6b75beb9b50c74a0c3fa96d48dac00823b
Kami Joseph <josephkiarie561@gmail.com>
2025-01-11 19:43:40 +0300

    Main Function for the binomial distribution Calculator

--- binomial_distribution.py
+++ binomial_distribution.py
@@ -4,7 +4,19 @@ def binomial_distributions():
     pass
 
 def main():
-    pass
+    print ("=== Binomial Distribution Calculator === \n")
+    print ("=== Solution must have a binary outcome ===")
+    print ("=== Trials should be indipendent ===")
+    print ("=== Number of trials should be included ===")
+    print ("=== The probability of each trial should be equal ===")
+    print ("=== The probability should be between 0 and 1 ===\n")
+    print("---------------------------------------------------------")
+    
+    calculate_bs = input("To calculate, press 1 and 2 to exit: ")
+    if calculate_bs == "1":
+        binomial_distributions()
+    else:
+        print("Goodbye! See you next time!")
 
 if __name__ == "__main__":
     main()
