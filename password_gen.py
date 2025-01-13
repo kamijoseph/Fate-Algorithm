4e461137b2a7a27aa270e0c7283495eb16875a90
Kami Joseph <josephkiarie561@gmail.com>
2025-01-13 17:26:31 +0300

    generator function

--- password_gen.py
+++ password_gen.py
@@ -3,8 +3,27 @@
 import string
 import random
 
-def passwd_gen():
-    pass
+def passwd_gen(length):
+    if length < 4:
+        raise ValueError("Password length must be atleast 4 characters.")
+
+    # Passwords characters Set
+    uppercase = string.ascii_uppercase
+    lowercase = string.ascii_lowercase
+    digits = string.digits
+    special_char = string.punctuation
+    
+    password = [
+        random.choice(uppercase),
+        random.choice(lowercase),
+        random.choice(digits),
+        random.choice(special_char)
+    ]
+    
+    allChars = uppercase + lowercase + digits + special_char
+    password += random.choices(allChars, k=length-4)
+    random.shuffle(password)
+    return ''.join(password)
 
 def main():
     pass
