e7a13c8227aa1176351d472b7bbeac3e123b05ed
Kami Joseph <josephkiarie561@gmail.com>
2025-01-16 15:58:49 +0300

    get players function

--- pigGame.py
+++ pigGame.py
@@ -3,7 +3,16 @@
 import random
 
 def getPlayers():
-    pass
+    while True:
+        players = input("Enter the number of players (2-4): ")
+        if players.isdigit():
+            players = int(players)
+            if 2 <= players <= 4:
+                return players
+            else:
+                print("Players must be between two and four")
+        else:
+            print("Invalid entry! Try again")
 
 def main():
     pass
