f22228ac880ff0a69f1f536232e2d9d69b7408d0
Kami Joseph <josephkiarie561@gmail.com>
2025-01-16 16:22:41 +0300

    rooooooooooooooooooooooooooooooooooooooll

--- pigGame.py
+++ pigGame.py
@@ -22,9 +22,25 @@ def main():
     # ZeGame looooooooop
     while max(playerScore) < maxScore:
         for player in range(players):
-            print(f"\nPlayer {player + 1}, its tour turn.")
+            print(f"\nPlayer {player + 1}, its your turn.")
             currentScore = 0
             
+            while True:
+                roll = input("Would you like to roll? (Yes/No): ").strip().lower()
+                if roll == "yes":
+                    value = random.randint(1, 6)
+                    print(f"You rolled a {value}")
+                    if value == 1:
+                        print("You rolled a 1. Turn over.")
+                        currentScore = 0
+                        break
+                    else:
+                        currentScore += value
+                        print(f"Your current score this turn is: {currentScore}")
+                elif roll == "No":
+                    break
+                else:
+                    print("Invalid input. Enter Yes to roll or No to hold.")
             
 if __name__ == "__main__":
     main()
