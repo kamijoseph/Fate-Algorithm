d5efd28c3283b641481ac94284dd18de48978524
Kami Joseph <josephkiarie561@gmail.com>
2025-01-16 16:55:57 +0300

    slotMachine initialized

---
+++ slot_machine.py
@@ -0,0 +1,48 @@
+
+# Slot Machine Game
+MAX_LINES = 3
+MAX_BETS = 100
+MIN_BETS = 1
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2,
+}
+
+def checkWins(columns, lines, bets, values):
+    pass
+
+def slotSpin(rows, cols, symbols):
+    pass
+
+def slotMachine(columns):
+    pass
+
+def deposit():
+    pass
+
+def getLines():
+    pass
+
+def bets():
+    pass
+
+def spin():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
