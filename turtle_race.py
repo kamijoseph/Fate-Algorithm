f9ab6249f9d25ab059f713d1c8ff6f5815b71dbe
Kami Joseph <josephkiarie561@gmail.com>
2025-01-17 14:45:57 +0300

    turtle racing game initialized

---
+++ turtle_race.py
@@ -0,0 +1,27 @@
+
+# Simple turtle racing game with tkinter
+import turtle
+import time
+import random
+
+WIDTH, HEIGHT = 500, 500
+COLORS = ['cyan', 'green', 'blue', 'orange', 'brown', 'red', 'purple', 'pink', 'yellow', 'black']
+
+def racersNumber():
+    pass
+
+def createTurtles(racers):
+    pass
+
+def race(colors):
+    pass
+
+def initializeTurtles():
+    pass
+
+def main():
+    racers = racersNumber()
+    initializeTurtles()
+
+if __name__ == "__main__":
+    main()
