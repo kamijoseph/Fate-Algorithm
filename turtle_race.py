015707118b50d442e20dd219fa0dc282fcf9674b
Kami Joseph <josephkiarie561@gmail.com>
2025-01-17 15:17:11 +0300

    turtle racers

--- turtle_race.py
+++ turtle_race.py
@@ -14,14 +14,28 @@ def createTurtles(racers):
     pass
 
 def race(colors):
-    pass
+    turtles = createTurtles(colors)
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+            
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+            
 
 def initializeTurtles():
-    pass
+    screen = turtle.Screen()
+    screen.setup(WIDTH,  HEIGHT)
+    screen.title("Racing Turtles")
 
-def main():
+def main(colors):
     racers = racersNumber()
     initializeTurtles()
+    random.shuffle(COLORS)
+    winner = race(colors)
+    print("The winner is the turtle with color, {winner}")
 
 if __name__ == "__main__":
     main()
